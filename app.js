2fb36f4b36b53ad2c50767f1bced213c97c71137
Jaesung Lee <jaesungdhl@gmail.com>
2019-04-23 00:35:48 -0700

    Add landing page that animates different camping images using background image slider on CSS. Create a route that routes to landing page using express

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const Express = require("express");
-const App = express();
+const App = Express();
 
+//Sets the view engine template into ejs on all HTML files.
+//for example, views/landing.ejs -> views/landing
 App.set("view engine", "ejs");
+//Sets to serve static files such as css, images, in a directory named "public"
+App.use(Express.static("public"));
 
 App.get("/", (req,res) => {
     res.render("landing");
 })
 
-App.listen(3003);
+App.listen(3003, () => {
+    console.log("Server has started running.")
+})
